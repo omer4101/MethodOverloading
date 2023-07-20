abb6888f3668c94cb3d11e73820f0a7482ccfecc
omerbariskayaalp <omerbariskayaalp@gmail.com>
2023-07-20 15:41:30 +0300

    MethodOverloading

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Sums state=new Sums();
+        System.out.println(state.sum(53,32));
+        System.out.println(state.sum(15.4,7.1));
+        System.out.println(state.sum("kek","w"));
+    }
+}
---
+++ src/Sums.java
@@ -0,0 +1,13 @@
+public class Sums {
+    public int sum(int number1,int number2){
+        return number1+number2;
+    }
+    public double sum(double number1,double number2){
+    return number1+number2;
+    }
+    public String sum(String number1,String number2){
+        return number1+number2;
+    }
+
+
+}
